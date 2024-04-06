f740374e33af183527115e373469e8d162048c7c
cooper <openbilibili@126.com>
2024-04-07 00:42:51 +0800

    chore: some change

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 # 确保脚本抛出遇到的错误
 set -e
@@ -21,4 +21,4 @@ git add -A
 git commit -m 'deploy to the gh-pages'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:AliceCooper214/nail-ui.git master:gh-pages
+git push -f https://github.com/AliceCooper214/nail-ui.git master:gh-pages
